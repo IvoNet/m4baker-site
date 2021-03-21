2b21f87b14536bbe1b2f22534b6a347558fa9ad3
Ivo Woltring <ivo@ivonet.nl>
2021-03-21 22:39:11 +0100

    - settings - added build and run scripts

---
+++ src/test/java/nl/ivonet/m4baker/M4BakerSiteTests.java
@@ -0,0 +1,13 @@
+package nl.ivonet.m4baker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4BakerSiteTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
