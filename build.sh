0ff99499362a44bb95965026bd4de59a7f515036
Ivo Woltring <ivo@ivonet.nl>
2021-03-21 22:36:04 +0100

    - settings - added build and run scripts

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+mvn clean package
+if [ "$?" -ne 0 ]; then
+    exit 1
+fi
+
+docker_name=192.168.2.3:5555
+image=m4baker
+version=$(<./VERSION)
+
+deploy="false"
+#deploy="true"
+versioning=false
+#versioning=true
+
+OPTIONS="$OPTIONS --no-cache"
+#OPTIONS="$OPTIONS --force-rm"
+
+docker build ${OPTIONS} -t $docker_name/${image}:latest .
+if [ "$?" -eq 0 ] && [ "${deploy}" == "true" ]; then
+    docker push $docker_name/${image}:latest
+else
+    exit 1
+fi
+
+if [ "$versioning" == "true" ]; then
+    docker tag $docker_name/${image}:latest $docker_name/${image}:${version}
+    if [ "$?" -eq 0 ] && [ ${deploy} == "true" ]; then
+        docker push $docker_name/${image}:${version}
+    fi
+fi
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 
+docker run -it --rm -p 8080:8080 -v /Volumes/development/M4Baker:/downloads 192.168.2.3:5555/m4baker
